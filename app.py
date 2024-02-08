21fd3f29e7b1b8e718c4a8d3e7861be600c18844
Blaze <111865477+geGDVS@users.noreply.github.com>
2024-02-08 11:49:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 import os
 
 app = Flask(__name__)
 
+text = "AWA"
+
 @app.route('/')
-def hello_world():
+def index():
     return 'AWA!'
+
+@app.route('/awaya', methods=['GET', 'POST'])
+def uptext():
+    global text
+    if request.method != "POST":
+        return render_template('index.html')
+    else:
+        if request.form["token"] == os.getenv("TOKEN"):
+            text = request.form['text']
+            return {"code": "200", "msg": "上传成功"}
+        else:
+            return {"code": "200", "msg": "token错误"}
+
+@app.route('/gettext')
+def gettext():
+    global text
+    return text
